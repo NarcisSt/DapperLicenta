da165e4615195ba66ed89f5a358eb1db1b84c65f
Barat Narcis-Stefan (uie46448) <narcis-stefan.barat@continental-corporation.com>
2022-06-02 00:52:59 +0300

    Final version

--- DapperLicenta/Repos/LicentaRepo.cs
+++ DapperLicenta/Repos/LicentaRepo.cs
@@ -2,6 +2,7 @@
 using DapperLicenta.Datas;
 using DapperLicenta.Models;
 using Npgsql;
+using StackExchange.Profiling;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -14,11 +15,13 @@ namespace DapperLicenta.Repos
         {
             var conn = new NpgsqlConnection("Host=127.0.0.1;Port=5432;Database=postgres;Username=postgres;Password=admin");
             conn.Open();
-            return conn;
+            return new StackExchange.Profiling.Data.ProfiledDbConnection(conn, MiniProfiler.Current);
         }
 
         public void InsertTeam(TeamData teamData)
         {
+            var mp = MiniProfiler.StartNew("InsertTeam");
+
             Team team = new Team()
             {
                 City = teamData.City,
@@ -30,57 +33,111 @@ namespace DapperLicenta.Repos
             using (var conn = OpenConnection())
             {
                 var querySQL = @"INSERT INTO licenta.teams (name, country, city, points) VALUES (@name, @country, @city, @points);";
-                conn.Execute(querySQL, teamData);
+                using (mp.Step("Execute query"))
+                {
+                    conn.Execute(querySQL, teamData);
+                }
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
             }
 
         }
 
         public void UpdateTeam(TeamData teamData)
         {
+            var mp = MiniProfiler.StartNew("UpdateTeam");
+
             using (var conn = OpenConnection())
             {
                 var sqlUpdate = "UPDATE licenta.teams SET points = @points WHERE name = @name";
-                conn.Execute(sqlUpdate, new { teamData.Points, teamData.Name});
+
+                using (mp.Step("Execute query"))
+                {
+                    conn.Execute(sqlUpdate, new { teamData.Points, teamData.Name });
+                }
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
             }
 
         }
 
         public void DeletePlayer(string position, long age)
         {
+            var mp = MiniProfiler.StartNew("DeletePlayer");
+
             using (var conn = OpenConnection())
             {
+
                 var sqlDelete = "DELETE FROM licenta.players WHERE position = @position AND age > @age";
-                conn.Execute(sqlDelete, new {position, age});
+
+                using (mp.Step("Execute query"))
+                {
+                    conn.Execute(sqlDelete, new { position, age });
+                }
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
             }
         }
 
         public int GetPlayerAndTeamByPosition(string position)
         {
+            int players;
+
+            var mp = MiniProfiler.StartNew("GetPlayerAndTeamByPosition");
+
             using (var conn = OpenConnection())
             {
                 var sqlDelete = @"SELECT players.*, teams.country, teams.city, teams.points FROM licenta.players INNER JOIN licenta.teams ON players.team = teams.name
-                       WHERE players.position = @position AND players.team LIKE '%United'"; 
+                       WHERE players.position = @position AND players.team LIKE '%United'";
 
-                return conn.Query<PlayerAndTeamData>(sqlDelete, new {position}).ToList().Count;
+                using (mp.Step("Execute query"))
+                {
+                    players = conn.Query<PlayerAndTeamData>(sqlDelete, new { position }).ToList().Count;
+                }
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
+                return players;
             }
 
         }
 
         public int GetAllPlayers()
         {
+            int players;
+
+            var mp = MiniProfiler.StartNew("GetAllPlayers");
+
             using (var conn = OpenConnection())
             {
                 var querySQL = @"SELECT * FROM licenta.players ORDER BY id ASC;";
-                return conn.Query<Player>(querySQL).ToList().Count;
+                using (mp.Step("Execute query"))
+                {
+                    players = conn.Query<Player>(querySQL).ToList().Count;
+                }
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
+                return players;
             }
         }
 
         public int GetTeamsByPoints(long points)
         {
+            int teams;
+
+            var mp = MiniProfiler.StartNew("GetTeamsByPoints");
+
             using (var conn = OpenConnection())
             {
                 var querySQL = @"SELECT * FROM licenta.teams WHERE teams.points > @points;";
-                return conn.Query<Team>(querySQL, new {points}).ToList().Count;
+
+                using (mp.Step("Execute query"))
+                {
+                    teams = conn.Query<Team>(querySQL, new { points }).ToList().Count;
+                }
+
+                mp.Stop();
+                Console.WriteLine(mp.RenderPlainText());
+                return teams;
             }
         }
 
